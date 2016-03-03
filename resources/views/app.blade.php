ae185860a102305fe939d7c9c442275fc9cfc114
seahouse <seahouse@126.com>
2016-03-03 16:58:26 +0800

    design app page.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page Title</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/jquery.mobile-1.4.5.min.css" />	
+</head>
+<body>
+	<div data-role="page">
+		<div data-role="header">
+			<h1>审批</h1>
+		</div>
+		<div data-role="main" class="ui-content">
+			<div data-role="navbar">
+				<ul>
+					<li><a href="#" class="ui-btn-active">待我审批</a></li>
+					<li><a href="#">我发起的</a></li>
+				</ul>
+			</div>
+			<p><a href="#bar">报销</a>  <a href="#bar">请款</a></p>
+			<p><a href="/app2">外部页面</a></p>
+		</div>
+		<div data-role="footer"></div>
+	</div>
+
+	<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/jquery.mobile-1.4.5.min.js"></script>
+</body>
+</html>
