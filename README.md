848f6b471e2adce1b78faee0566b46c93d9cb912
weimm <weimm@home>
2016-04-07 22:40:54 +0800

    Add manual to set url into github, so that it can auto deploy the website.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hyerp.
 test git pull url.
+
+
+Add below url to GitHub Settings / Webhooks / Add webhook:
+    http://hyerp.ricki.cn/gitpull
+Just the push event.
--- resources/views/gitpull.php
+++ resources/views/gitpull.php
@@ -1,5 +1,8 @@
- <h2>Please make sure the permission: "chmod -Rf g+w hyerp"</h2>
+ <p>*If failed, please check the permission: "chmod -Rf g+w hyerp"</p>
+ <br>
+ <p>----git pull start----</p>
  <?php
    $output = shell_exec('cd /home/liangyi/hyerp && git pull');
    echo "<pre>$output</pre>";
  ?>
+ <p>-----git pull end-----</p>
