f5838ecc8e26f91f12fac1232080bbe3bec673b3
seahouse <seahouse@126.com>
2016-06-13 20:18:53 +0800

    test

--- resources/views/approval/reimbursementapprovals/mcreate.blade.php
+++ resources/views/approval/reimbursementapprovals/mcreate.blade.php
@@ -34,7 +34,7 @@
 			    timeStamp: {!! array_get($config, 'timeStamp') !!}, // 必填，生成签名的时间戳
 			    nonceStr: '{!! array_get($config, 'nonceStr') !!}', // 必填，生成签名的随机串
 			    signature: '{!! array_get($config, 'signature') !!}', // 必填，签名
-			    jsApiList: ['biz.util.uploadImage'] // 必填，需要使用的jsapi列表
+			    jsApiList: ['biz.ding.post'] // 必填，需要使用的jsapi列表
 			});
 
 			dd.ready(function() {
