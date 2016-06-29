1c45aad919c83d6ac745ede8a0352cace50df932
seahouse <seahouse@126.com>
2016-06-29 13:54:25 +0800

    ui design

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>@yield('title')</title>
-	<link href="{{ asset("//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css") }}" rel="stylesheet">
+<!--	<link href="{{ asset("//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css") }}" rel="stylesheet"> -->
+	<link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
 </head>
 <body>
 	@yield('main')
--- resources/views/mapproval.blade.php
+++ resources/views/mapproval.blade.php
@@ -6,10 +6,13 @@
 	<p>
 		<div class="btn-group btn-group-justified" role="group" aria-label="...">
 			<div class="btn-group" role="group">
-				<a href="/approval/reimbursements/mindexmyapproval"><button type="button" class="btn btn-default">待我审批</button></a>
+<!--
+				<a href="/approval/reimbursements/mindexmyapproval"><button type="button" class="btn btn-default btn-lg">待我审批</button></a>
+-->
+				<a href="/approval/reimbursements/mindexmyapproval"><button type="button" class="btn btn-default btn-lg btn-menu-1">待我审批</button></a>
 			</div>
 			<div class="btn-group" role="group">
-				<a href="/approval/reimbursements/mindexmy"><button type="button" class="btn btn-default">我发起的</button></a>
+				<a href="/approval/reimbursements/mindexmy"><button type="button" class="btn btn-default btn-lg">我发起的</button></a>
 			</div>
 		</div>
 	</p>
