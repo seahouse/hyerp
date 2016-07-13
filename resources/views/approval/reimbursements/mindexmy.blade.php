47e97ac004f3a66c8cd1eb2832a2be8a6a4c03de
jayzhou <ljchow@163.com>
2016-07-13 16:14:16 +0800

    change the head photo when there's a head

--- resources/views/approval/reimbursements/mindexmy.blade.php
+++ resources/views/approval/reimbursements/mindexmy.blade.php
@@ -24,23 +24,21 @@
             <a href="{{ url('/approval/reimbursements/mshow', $reimbursement->id) }}" class="list-group-item">
                 {{-- 以下的代码判断说明：如果用户的头像url为空，则以名字显示，否则以这个头像url来显示图片 --}}
                 @if (Auth::user()->dingtalkGetUser()->avatar == '')
-                    {{ Auth::user()->dingtalkGetUser()->name }}
+                    <div class='col-xs-2 col-sm-2 name'>{{ Auth::user()->dingtalkGetUser()->name }}</div>
                 @else
-                    {{ Auth::user()->dingtalkGetUser()->avatar }}
+                    <div class='col-xs-2 col-sm-2'><img class="name img" src="{{ Auth::user()->dingtalkGetUser()->avatar }}" /></div>
                 @endif
-                <div class='col-xs-2 col-sm-2 name'>梁毅</div>
-                {{ Auth::user()->dingtalkGetUser()->avatar }}
                 <div class='col-xs-7 col-sm-7 content'>
                     <div title="{{ $reimbursement->applicant->name }}的报销" class="title">{{ $reimbursement->applicant->name }}的报销</div>
                     {{-- 以下的代码判断说明：如果审批id大于0，则显示“待审批”，否则显示“审批完成”。 --}}
                     {{-- 当状态为“审批完成”时，字体为灰色 --}}
                     @if ($reimbursement->approversetting_id > 0)
-                        <div class="status">待审批</div>
+                        <div class="statusTodo">待审批</div>
                     @else
-                        <div class="status">审批完成</div>      {{-- 此时，字体要修改为灰色 --}}
+                        <div class="statusDone">审批完成</div>      {{-- 此时，字体要修改为灰色 --}}
                     @endif
                 </div>
-                <div class='col-xs-3 col-sm-3'><span class="time">{{ $reimbursement->created_at }}</span></div>
+                <div class='col-xs-3 col-sm-3 time'><span >{{ $reimbursement->created_at }}</span></div>
             </a>
         </div>     
         @endforeach
