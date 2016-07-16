0b079b98bbe7e54cf20fba789452e1d7fcb5e6c3
jayzhou <ljchow@163.com>
2016-07-16 09:55:26 +0800

    add the styles of the tab

--- resources/views/approval/reimbursements/mindexmyapproval_nav.blade.php
+++ resources/views/approval/reimbursements/mindexmyapproval_nav.blade.php
@@ -3,16 +3,22 @@
 @section('title', '待我审批')
 
 @section('main')
-<p>
-	<div class="btn-group btn-group-justified" role="group" aria-label="...">
-	    <div class="btn-group" role="group">
-	        <a href="/approval/reimbursements/mindexmyapproval"><button type="button" class="btn btn-default">待我审批的</button></a>
+<div class="apprNav">
+	<div class="btn-group btn-group-justified wrapper" role="group" aria-label="...">
+	    <div class="btn-group btnw" role="group">
+	        <a href="/approval/reimbursements/mindexmyapproval">
+	        	<div class="{{strpos($_SERVER['REQUEST_URI'], 'mindexmyapprovaled') == '' ? 'text selected' : 'text' }}">待我审批的</div>
+	        </a>
 	    </div>
-	    <div class="btn-group" role="group">
-	        <a href="/approval/reimbursements/mindexmyapprovaled"><button type="button" class="btn btn-default">我已审批的</button></a>
+	    <div class="btn-group btnw" role="group">
+	        <a href="/approval/reimbursements/mindexmyapprovaled">
+	        	<div class="{{strpos($_SERVER['REQUEST_URI'], 'mindexmyapprovaled') > 0 ? 'text selected' : 'text' }}">我已审批的</div>
+	        </a>
 	    </div>
 	</div>
-</p>
+</div>
+<script type="text/javascript">
+</script>
 <p>
 @yield('mindexmyapproval_main')
 </p>
