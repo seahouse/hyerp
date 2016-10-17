4f5de773a75885b63c6a99a7f9a4696ce71ee247
seahouse <seahouse@126.com>
2016-10-17 16:33:44 +0800

    test.

--- resources/views/approval/_list.blade.php
+++ resources/views/approval/_list.blade.php
@@ -3,11 +3,14 @@
     <div class="reimbList list-group">
         <a href="{{ url($item->url . $item->id . $href_suffix) }}" class="list-group-item">
             {{-- 以下的代码判断说明：如果用户的头像url为空，则以名字显示，否则以这个头像url来显示图片 --}}
+            <div class='col-xs-2 col-sm-2'>{{ Auth::user()->dingtalkGetUser()->name }}</div>
+{{--
             @if (Auth::user()->dingtalkGetUser()->avatar == '')
-                <div class='col-xs-2 col-sm-2 name'>{{ Auth::user()->dingtalkGetUser()->name }}</div>
+                <div class='col-xs-2 col-sm-2'>{{ Auth::user()->dingtalkGetUser()->name }}</div>
             @else
                 <div class='col-xs-2 col-sm-2'><img class="name img" src="{{ Auth::user()->dingtalkGetUser()->avatar }}" /></div>
             @endif
+--}}
             <div class='col-xs-7 col-sm-7 content'>
                 <div title="{{ $item->applicant_name }}的报销" class="title">{{ $item->applicant_name }}的{{ $item->type }}</div>
                 {{-- 以下的代码判断说明：如果审批id大于0，则显示“待审批”，否则显示“审批完成”。 --}}
