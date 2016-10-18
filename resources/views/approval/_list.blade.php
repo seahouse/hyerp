046e829294552a641c48dfa7acf4cbecdf92b6ba
seahouse <seahouse@126.com>
2016-10-18 10:39:13 +0800

    tt

--- resources/views/approval/_list.blade.php
+++ resources/views/approval/_list.blade.php
@@ -3,14 +3,12 @@
     <div class="reimbList list-group">
         <a href="{{ url($item->url . $item->id . $href_suffix) }}" class="list-group-item">
             {{-- 以下的代码判断说明：如果用户的头像url为空，则以名字显示，否则以这个头像url来显示图片 --}}
-            <div class='col-xs-2 col-sm-2'>{{ Auth::user()->dingtalkGetUser()->name }}</div>
-{{--
-            @if (Auth::user()->dingtalkGetUser()->avatar == '')
-                <div class='col-xs-2 col-sm-2'>{{ Auth::user()->dingtalkGetUser()->name }}</div>
+            @if ($dduser->avatar == '')
+                <div class='col-xs-2 col-sm-2'>{{ $dduser->name }}</div>
             @else
-                <div class='col-xs-2 col-sm-2'><img class="name img" src="{{ Auth::user()->dingtalkGetUser()->avatar }}" /></div>
+                <div class='col-xs-2 col-sm-2'><img class="name img" src="{{ $dduser->avatar }}" /></div>
             @endif
---}}
+
             <div class='col-xs-7 col-sm-7 content'>
                 <div title="{{ $item->applicant_name }}的报销" class="title">{{ $item->applicant_name }}的{{ $item->type }}</div>
                 {{-- 以下的代码判断说明：如果审批id大于0，则显示“待审批”，否则显示“审批完成”。 --}}
@@ -23,6 +21,6 @@
             </div>
             <div class='col-xs-3 col-sm-3 time'><span >{{ $item->created_at }}</span></div>
         </a>
-    </div>     
+    </div>
     @endforeach
 @endif
