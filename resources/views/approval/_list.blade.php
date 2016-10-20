9f9ba0a5be70646c769749af14e263b653afe5d1
seahouse <seahouse@126.com>
2016-10-20 09:48:23 +0800

    modify col-xs-x value.

--- resources/views/approval/_list.blade.php
+++ resources/views/approval/_list.blade.php
@@ -4,12 +4,12 @@
         <a href="{{ url($item->url . $item->id . $href_suffix) }}" class="list-group-item">
             {{-- 以下的代码判断说明：如果用户的头像url为空，则以名字显示，否则以这个头像url来显示图片 --}}
             @if ($dduser->avatar == '')
-                <div class='col-xs-2 col-sm-2'>{{ $dduser->name }}</div>
+                <div class='col-xs-3 col-sm-2 name'>{{ $dduser->name }}</div>
             @else
-                <div class='col-xs-2 col-sm-2'><img class="name img" src="{{ $dduser->avatar }}" /></div>
+                <div class='col-xs-3 col-sm-2'><img class="name img" src="{{ $dduser->avatar }}" /></div>
             @endif
 
-            <div class='col-xs-7 col-sm-7 content'>
+            <div class='col-xs-6 col-sm-7 content'>
                 <div title="{{ $item->applicant_name }}的报销" class="title">{{ $item->applicant_name }}的{{ $item->type }}</div>
                 {{-- 以下的代码判断说明：如果审批id大于0，则显示“待审批”，否则显示“审批完成”。 --}}
                 {{-- 当状态为“审批完成”时，字体为灰色 --}}
--- resources/views/approval/_list2.blade.php
+++ resources/views/approval/_list2.blade.php
@@ -4,9 +4,9 @@
         <a href="{{ url($href_pre_paymentrequest . $item->id . $href_suffix) }}" class="list-group-item">
             {{-- 以下的代码判断说明：如果用户的头像url为空，则以名字显示，否则以这个头像url来显示图片 --}}
             @if ($item->applicant->dt_avatar == '')
-                <div class='col-xs-2 col-sm-2'>{{ $item->applicant->name }}</div>
+                <div class='col-xs-3 col-sm-2 name'>{{ $item->applicant->name }}</div>
             @else
-                <div class='col-xs-2 col-sm-2'><img class="name img" src="{{ $item->applicant->dt_avatar }}" /></div>
+                <div class='col-xs-3 col-sm-2'><img class="name img" src="{{ $item->applicant->dt_avatar }}" /></div>
             @endif
 {{--
             @if (null != $item->applicant->dingtalkGetUser())
@@ -19,7 +19,7 @@
                 <div class='col-xs-2 col-sm-2 name'>{{ $item->applicant->name }}</div>
             @endif
 --}}
-            <div class='col-xs-7 col-sm-7 content'>
+            <div class='col-xs-6 col-sm-7 content'>
                 <div title="{{ $item->applicant->name }}的付款" class="title">{{ $item->applicant->name }}的付款</div>
                 {{-- 以下的代码判断说明：如果审批id大于0，则显示“待审批”，否则显示“审批完成”。 --}}
                 {{-- 当状态为“审批完成”时，字体为灰色 --}}
@@ -42,9 +42,9 @@
         <a href="{{ url($href_pre . $item->id . $href_suffix) }}" class="list-group-item">
             {{-- 以下的代码判断说明：如果用户的头像url为空，则以名字显示，否则以这个头像url来显示图片 --}}
             @if ($item->applicant->dt_avatar == '')
-                <div class='col-xs-2 col-sm-2'>{{ $item->applicant->name }}</div>
+                <div class='col-xs-3 col-sm-2 name'>{{ $item->applicant->name }}</div>
             @else
-                <div class='col-xs-2 col-sm-2'><img class="name img" src="{{ $item->applicant->dt_avatar }}" /></div>
+                <div class='col-xs-3 col-sm-2'><img class="name img" src="{{ $item->applicant->dt_avatar }}" /></div>
             @endif
 {{--
             @if (null != $item->applicant->dingtalkGetUser())
@@ -57,7 +57,7 @@
                 <div class='col-xs-2 col-sm-2 name'>{{ $item->applicant->name }}</div>
             @endif
 --}}
-            <div class='col-xs-7 col-sm-7 content'>
+            <div class='col-xs-6 col-sm-7 content'>
                 <div title="{{ $item->applicant->name }}的报销" class="title">{{ $item->applicant->name }}的报销</div>
                 {{-- 以下的代码判断说明：如果审批id大于0，则显示“待审批”，否则显示“审批完成”。 --}}
                 {{-- 当状态为“审批完成”时，字体为灰色 --}}
