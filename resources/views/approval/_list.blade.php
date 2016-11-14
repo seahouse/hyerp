7539a4a5187626b5bd9ebd8ba7683f03bb4a527c
seahouse <seahouse@126.com>
2016-11-14 20:43:20 +0800

    modify list style.

--- resources/views/approval/_list.blade.php
+++ resources/views/approval/_list.blade.php
@@ -10,36 +10,41 @@
         <a href="{{ url($href_pre_paymentrequest . $item->id . $href_suffix) }}" class="list-group-item">
             {{-- 以下的代码判断说明：如果用户的头像url为空，则以名字显示，否则以这个头像url来显示图片 --}}  
             @if (isset($dtuser->avatar))
-                <div class='col-xs-2 col-sm-2'><img class="name img" src="{{ $dtuser->avatar }}" /></div>
+                <div class='col-xs-3 col-sm-3 headIcon'><img class="name img" src="{{ $dtuser->avatar }}" /></div>
             @else
-                <div class='col-xs-2 col-sm-2 name'>{{ $dtuser->name }}</div>
+                <div class='col-xs-3 col-sm-3 name headIcon'>{{ $dtuser->name }}</div>
             @endif
 {{--
             @if ($dduser->avatar == '')
-                <div class='col-xs-2 col-sm-2 name'>{{ $dduser->name }}</div>
+                <div class='col-xs-3 col-sm-3 name headIcon'>{{ $dduser->name }}</div>
             @else
-                <div class='col-xs-2 col-sm-2'><img class="name img" src="{{ $dduser->avatar }}" /></div>
+                <div class='col-xs-3 col-sm-3 headIcon' ><img class="name img" src="{{ $dduser->avatar }}" /></div>
             @endif
 --}}
-            <div class='col-xs-7 col-sm-7 content'>
+            <div class='col-xs-6 col-sm-6 content'>
                 <div title="{{ $item->applicant_name }}的付款" class="title">
-                    <div class='longText'>{{ $item->applicant->name }}的付款 | {{ $item->amount }}</div>
+                    <div class='longText'>{{ $item->paymenttype }} | {{ $item->amount }}</div>
                     {{-- 示例：山东奥博环保科技有限公司 --}}
+                    {{-- @if (isset($item->supplier_hxold->name)) {{ $item->supplier_hxold->name }} @endif --}}
                     <div class='longText'>@if (isset($item->supplier_hxold->name)) {{ $item->supplier_hxold->name }} @endif</div>
+                    {{-- 示例：浙江锦润机电成套设备有限公司 --}}
+	                {{-- @if (isset($item->purchaseorder_hxold->custinfo_name)) {{ $item->purchaseorder_hxold->custinfo_name }} @endif --}}
+	                <div class='longText'>@if (isset($item->purchaseorder_hxold->custinfo_name)) {{ $item->purchaseorder_hxold->custinfo_name }} @endif</div>
+	                {{-- 示例：高密垃圾焚烧发电项目1#2#炉烟气脱硫净化装置系统工程 --}}
+	                {{-- @if (isset($item->purchaseorder_hxold->sohead_descrip)) {{ $item->purchaseorder_hxold->sohead_descrip }} @endif--}}
+	                <div class='longText'>@if (isset($item->purchaseorder_hxold->sohead_descrip)) {{ $item->purchaseorder_hxold->sohead_descrip }} @endif</div>
                 </div>
-                {{-- 示例：浙江锦润机电成套设备有限公司 --}}
-                <div class='longText'>@if (isset($item->purchaseorder_hxold->custinfo_name)) {{ $item->purchaseorder_hxold->custinfo_name }} @endif</div>
-                {{-- 示例：高密垃圾焚烧发电项目1#2#炉烟气脱硫净化装置系统工程 --}}
-                <div class='longText'>@if (isset($item->purchaseorder_hxold->sohead_descrip)) {{ $item->purchaseorder_hxold->sohead_descrip }} @endif</div>
-                {{-- 以下的代码判断说明：如果审批id大于0，则显示“待审批”，否则显示“审批完成”。 --}}
-                {{-- 当状态为“审批完成”时，字体为灰色 --}}
-                @if ($item->approversetting_id > 0)
+                
+                
+            </div>
+            <div class='col-xs-3 col-sm-3 time'>
+            	<span >{{ $item->created_at }}</span><br/>
+            	@if ($item->approversetting_id > 0)
                     <div class="statusTodo">待审批</div>
                 @else
                     <div class="statusDone">审批完成</div>      {{-- 此时，字体要修改为灰色 --}}
                 @endif
             </div>
-            <div class='col-xs-3 col-sm-3 time'><span >{{ $item->created_at }}</span></div>
         </a>
     </div>
     @endforeach
--- resources/views/approval/_list2.blade.php
+++ resources/views/approval/_list2.blade.php
@@ -4,9 +4,9 @@
         <a href="{{ url($href_pre_paymentrequest . $item->id . $href_suffix) }}" class="list-group-item">
             {{-- 以下的代码判断说明：如果用户的头像url为空，则以名字显示，否则以这个头像url来显示图片 --}}
             @if (isset($item->applicant->dtuser->avatar))
-                <div class='col-xs-2 col-sm-2'><img class="name img" src="{{ $item->applicant->dtuser->avatar }}" /></div>
+                <div class='col-xs-3 col-sm-3 headIcon'><img class="name img" src="{{ $item->applicant->dtuser->avatar }}" /></div>
             @else
-                <div class='col-xs-2 col-sm-2 name'>{{ $item->applicant->name }}</div>
+                <div class='col-xs-3 col-sm-3 name headIcon'>{{ $item->applicant->name }}</div>
             @endif
 {{--
             @if (null != $item->applicant->dingtalkGetUser())
@@ -19,11 +19,17 @@
                 <div class='col-xs-2 col-sm-2 name'>{{ $item->applicant->name }}</div>
             @endif
 --}}
-            <div class='col-xs-7 col-sm-7 content'>
+            <div class='col-xs-6 col-sm-6 content'>
                 <div title="{{ $item->applicant->name }}的付款" class="title">
-                    <div class='longText'>{{ $item->applicant->name }}的付款 | {{ $item->amount }} | @if (isset($item->supplier_hxold->name)) {{ $item->supplier_hxold->name }} @endif</div>
+                    <div class='longText'>{{ $item->applicant->name }}的付款 | {{ $item->amount }} </div>
+                    <div class='longText'>@if (isset($item->supplier_hxold->name)) {{ $item->supplier_hxold->name }} @endif</div>
+                    <div class='longText'>@if (isset($item->purchaseorder_hxold->custinfo_name)) {{ $item->purchaseorder_hxold->custinfo_name }} @endif</div>
+                    <div class='longText'>@if (isset($item->purchaseorder_hxold->sohead_descrip)) {{ $item->purchaseorder_hxold->sohead_descrip }} @endif</div>
                 </div>
-                <div class='longText'>@if (isset($item->purchaseorder_hxold->sohead_descrip)) {{ $item->purchaseorder_hxold->custinfo_name }} | {{ $item->purchaseorder_hxold->sohead_descrip }} @endif</div>
+
+            </div>
+            <div class='col-xs-3 col-sm-3 time'>
+                <span >{{ $item->created_at }}</span><br/>
                 {{-- 以下的代码判断说明：如果审批id大于0，则显示“待审批”，否则显示“审批完成”。 --}}
                 {{-- 当状态为“审批完成”时，字体为灰色 --}}
                 @if ($item->approversetting_id > 0)
@@ -32,7 +38,6 @@
                     <div class="statusDone">审批完成</div>      {{-- 此时，字体要修改为灰色 --}}
                 @endif
             </div>
-            <div class='col-xs-3 col-sm-3 time'><span >{{ $item->created_at }}</span></div>
         </a>
     </div>     
     @endforeach
