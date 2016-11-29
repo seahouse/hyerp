72e0207657aa99d8480eed7baaaf73028cfb7233
seahouse <seahouse@126.com>
2016-11-29 12:43:13 +0800

    fix.

--- resources/views/_back.blade.php
+++ resources/views/_back.blade.php
@@ -1,6 +1,11 @@
-@if (Auth::user()->email == "admin@admin.com")
+@if (isset(Auth::user()->email) and Auth::user()->email == "admin@admin.com")
 	@if ($_SERVER['REQUEST_URI'] == '/mapproval')
+	@elseif ($_SERVER['REQUEST_URI'] == '/mddauth')
 	@else
 	<input name='ht' type="button" onclick="javascript:history.go(-1)" value="后退" class="btn"></input>
 	@endif
 @endif
+
+<script type="text/javascript">
+	console.log("{{ $_SERVER['REQUEST_URI'] }}");
+</script>
