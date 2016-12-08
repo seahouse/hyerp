707dce85402eeb9852a90cd492463c10563e1d5a
seahouse <seahouse@126.com>
2016-12-08 13:15:03 +0800

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -44,7 +44,7 @@ Route::group(['middleware' => ['web']], function () {
 	Route::get('dingtalk/getuserinfo/{code}', 'DingTalkController@getuserinfo');
     Route::get('dingtalk/getconfig', 'DingTalkController@getconfig');
     Route::post('dingtalk/register_call_back', 'DingTalkController@register_call_back');
-    Route::post('dingtalk/test', 'DingTalkController@test');
+    Route::post('dingtalk/receive', 'DingTalkController@receive');
 
     // run .bat shell command to run git pull.
     Route::get('gitpullbybat', function() { return view('gitpullbybat'); });
