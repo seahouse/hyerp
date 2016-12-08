d524a9fd43d78ba4702da266c4c56e0a48060d66
seahouse <seahouse@126.com>
2016-12-08 10:48:38 +0800

    test

--- app/Http/Controllers/DingTalkController.php
+++ app/Http/Controllers/DingTalkController.php
@@ -425,7 +425,7 @@ class DingTalkController extends Controller
             'call_back_tag' => ['user_modify_org'],
             'token' => str_random(32),
             'aes_key' => str_random(43),
-            'url' => 'www.dingtalk.com'
+            'url' => url('system/users/test')
         ];
         // dd(url('system/users/test'));
 
