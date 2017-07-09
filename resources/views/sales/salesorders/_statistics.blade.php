41690b11b7f05ac9ef724649caf1afe17c9535b8
seahouse <seahouse@126.com>
2017-07-09 08:21:19 +0800

    fix.

--- resources/views/sales/salesorders/_statistics.blade.php
+++ resources/views/sales/salesorders/_statistics.blade.php
@@ -1,27 +1,25 @@
+@if (Auth::user()->isSuperAdmin())
+	<div id="tabid1" class="tabid">
+		@if ($sohead)
+			<p>销售订单编号：{{ $sohead->number }}</p>
+			<p>工程名称：{{ $sohead->descrip }}</p>
+			<p>客户：{{ $sohead->custinfo->name }}</p>
+			<p>订单金额：{{ $sohead->amount }}万</p>
+			<p>订单收款总金额：{{ $sohead->receiptpayments->sum('amount') }}万</p>
+			<?php $pohead_amount_total = $sohead->poheads->sum('amount'); ?>
+			<p>对应的采购订单合同金额总额：{{ number_format($pohead_amount_total / 10000.0, 4) }}万</p>		{{-- 似乎写到数据库视图中速度更快 --}}
+			@if ($sohead->amount > 0.0)
+				<p>采购成本比例：{{ number_format($pohead_amount_total / ($sohead->amount * 10000.0) * 100.0, 2) }}%</p>
+			@else
+				<p>采购成本比例：-</p>
+			@endif
+			<?php $pohead_amount_payment_total = $sohead->payments->sum('amount'); ?>
+			<p>采购订单付款总金额：{{ number_format($pohead_amount_payment_total / 10000.0, 4) }}万</p>
+			<p>采购订单累计付款比例：{{ number_format($pohead_amount_payment_total / $pohead_amount_total * 100.0, 2) }}%</p>
 
-<div id="tabid1" class="tabid">
-	@if ($sohead)
-		<p>销售订单编号：{{ $sohead->number }}</p>
-		<p>工程名称：{{ $sohead->descrip }}</p>
-		<p>客户：{{ $sohead->custinfo->name }}</p>
-		<p>订单金额：{{ $sohead->amount }}万</p>
-		<p>订单收款总金额：{{ $sohead->receiptpayments->sum('amount') }}万</p>
-        <?php $pohead_amount_total = $sohead->poheads->sum('amount'); ?>
-		<p>对应的采购订单合同金额总额：{{ $pohead_amount_total }}</p>		{{-- 似乎写到数据库视图中速度更快 --}}
-		@if ($sohead->amount > 0.0)
-			<p>采购成本比例：{{ $pohead_amount_total / ($sohead->amount * 10000.0) * 100.0 }}%</p>
-		@else
-			<p>采购成本比例：-</p>
 		@endif
-        <?php $pohead_amount_payment_total = $sohead->payments->sum('amount'); ?>
-		<p>采购订单付款总金额：{{ $pohead_amount_payment_total }}</p>
-		<p>采购订单累计付款比例：{{ $pohead_amount_payment_total / $pohead_amount_total * 100.0 }}%</p>
-
-
-
-	@endif
-</div>
-
+	</div>
+@endif
 
 
 
