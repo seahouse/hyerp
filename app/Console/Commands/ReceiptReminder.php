881d91907290ddb5e99525a574a7e9a221a05a7b
seahouse <seahouse@126.com>
2017-10-28 12:10:35 +0800

    fix.

--- app/Console/Commands/ReceiptReminder.php
+++ app/Console/Commands/ReceiptReminder.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\DingTalkController;
 use App\Models\Sales\Paywayass_hxold;
 use App\Models\Sales\Salesorder_hxold;
 use App\Models\System\User;
+use App\Models\System\Userold;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Log;
@@ -17,7 +18,7 @@ class ReceiptReminder extends Command
      *
      * @var string
      */
-    protected $signature = 'reminder:receipt {useremail=admin@admin.com}';
+    protected $signature = 'reminder:receipt {useremail=admin@admin.com} {--nottomanager= : Do not send message to sales manager}';
 
     /**
      * The console command description.
@@ -48,17 +49,23 @@ class ReceiptReminder extends Command
         foreach ($soheads as $sohead)
         {
             $this->info($sohead->id);
+//            if ($sohead->id != 7527 && $sohead->id != 7538)
+//                continue;
+
             $receivedAmount = $sohead->receiptpayments()->sum('amount');
             $msgList = [];
+            $toWuHL = false;
             // 获取付款方式
-            $paywayasses = Paywayass_hxold::where('paywayass_order_id', $sohead->id)->get();
+            $paywayasses = Paywayass_hxold::where('paywayass_order_id', $sohead->id)->orderBy('payway_seq')->get();
             $percentSum = 0.0;
             foreach ($paywayasses as $paywayass)
             {
                 $this->info('  ' . $paywayass->paywayass_id);
                 $paywayId = $paywayass->paywayass_payway_id;
                 $percentSum += $paywayass->paywayass_value;
+//                Log::info("percentSum: " . $percentSum);
 
+                $bWarning = false;
                 $amountDest = $sohead->amount * $percentSum;
                 $notReceivedAmount = $amountDest - $receivedAmount;
 //                Log::info("notReceivedAmount: " . $notReceivedAmount);
@@ -68,21 +75,51 @@ class ReceiptReminder extends Command
                 {
                     case 2:
                     case 3:
+                    case 4:
                         // 目前数据库中的电气部似乎都没有显示完成，所以先不考虑电气部的完成情况
 //                        if ($sohead->techdept_status == 1 && $sohead->elecdept_status == "完成")
                         if ($sohead->techdept_status == 1)
                         {
+                            $bWarning = true;
                             $this->info('      ' . "design finished.");
                         }
                         break;
+                    case 13:
                     case 5:
                         if ($sohead->delivery_status == 1)
                         {
+                            $bWarning = true;
                             $this->info('      ' . "delivery finished.");
                             Log::info("delivery finished.");
                         }
                         break;
-                    case 7:
+                    case 14:
+                    case 7:             // 调试后: 项目投运日期（72+24小时完成日）
+                    case 8:
+                        // Carbon使用方法: https://9iphp.com/web/laravel/php-datetime-package-carbon.html
+                        $this->info('      ' . $sohead->debugend_date);
+                        $debugendDate = Carbon::parse($sohead->debugend_date);
+                        $this->info('      ' . $debugendDate);
+                        $baseDate = Carbon::create(1900, 1, 1);
+                        if ($sohead->id == 7538)
+                        {
+//                            Log::info($sohead->debugend_date);
+//                            Log::info($debugendDate);
+//                            Log::info($baseDate);
+//                            Log::info($amountDest);
+//                            Log::info($receivedAmount);
+//                            Log::info($notReceivedAmount);
+                        }
+                        if ($debugendDate->gt($baseDate))
+                        {
+                            $bWarning = true;
+                            $this->info('      ' . "debug finished.");
+                            Log::info("debug finished.");
+                        }
+                        break;
+                    case 9:             //
+                    case 12:            // 环保验收: 项目投运日期（72+24小时完成日）后, 6个月后
+                    case 11:
                         // Carbon使用方法: https://9iphp.com/web/laravel/php-datetime-package-carbon.html
                         $this->info('      ' . $sohead->debugend_date);
                         $debugendDate = Carbon::parse($sohead->debugend_date);
@@ -94,8 +131,28 @@ class ReceiptReminder extends Command
                             Log::info($debugendDate);
                             Log::info($baseDate);
                         }
-                        if ($debugendDate->gt($baseDate))
+                        if ($debugendDate->gt($baseDate) && Carbon::now()->gt($debugendDate->addMonth(6)))
                         {
+                            $bWarning = true;
+                            $this->info('      ' . "debug finished.");
+                            Log::info("debug finished.");
+                        }
+                        break;
+                    case 10:        // 质保: 项目投运日期（72+24小时完成日）后, 12个月后
+                        // Carbon使用方法: https://9iphp.com/web/laravel/php-datetime-package-carbon.html
+                        $this->info('      ' . $sohead->debugend_date);
+                        $debugendDate = Carbon::parse($sohead->debugend_date);
+                        $this->info('      ' . $debugendDate);
+                        $baseDate = Carbon::create(1900, 1, 1);
+                        if ($sohead->id == 7545)
+                        {
+                            Log::info($sohead->debugend_date);
+                            Log::info($debugendDate);
+                            Log::info($baseDate);
+                        }
+                        if ($debugendDate->gt($baseDate) && Carbon::now()->gt($debugendDate->addMonth(12)))
+                        {
+                            $bWarning = true;
                             $this->info('      ' . "debug finished.");
                             Log::info("debug finished.");
                         }
@@ -104,21 +161,29 @@ class ReceiptReminder extends Command
                         ;
                 }
 
-                if ($notReceivedAmount > 0.0)
+                if ($bWarning && $notReceivedAmount > 0.0)
                 {
                     $msgTemp = "应收" . $paywayass->payway_name . "款" . $amountDest . "万, " .
                         "实收" . $receivedAmount . "万, 未收" . $notReceivedAmount . "万";
                     Log::info($msgTemp);
+                    $msgTemp = "累计可收" . $amountDest . "万, 累计实收" . $receivedAmount . "万, 差" . $notReceivedAmount . "万.";
+                    if ($notReceivedAmount > 50.0)
+                        $toWuHL = true;
                     array_push($msgList, $msgTemp);
                 }
             }
 
             if (count($msgList) > 0)
             {
-                $msg = "客户为" . $sohead->custinfo_name . "的" . $sohead->descrip . "项目, " .
-                    "该订单金额为" . $sohead->amount . "万, 于" . $sohead->orderdate . "签订. " .
-                    implode(',', $msgList) .
-                    ", 请抓紧催收";
+//                $msg = ($sohead->projectjc == "" ? $sohead->descrip : $sohead->projectjc) . ", "  .
+//                    "合同" . $sohead->amount . "万, 于" . $sohead->orderdate . "签订. " .
+//                    implode(',', $msgList) .
+//                    ", 请抓紧催收. 1";
+//                $msg = ($sohead->projectjc == "" ? $sohead->descrip : $sohead->projectjc) . ", "  .
+//                    "合同" . $sohead->amount . "万, 累计可收" . $amountDest . "万, 累计实收" . $receivedAmount .
+//                    "万, 差" . $notReceivedAmount . "万. 2";
+                $msg = ($sohead->projectjc == "" ? $sohead->descrip : $sohead->projectjc) . ", "  .
+                    "合同" . $sohead->amount . "万, " . array_pop($msgList) . "";
 
                 Log::info($msg);
 
@@ -127,6 +192,33 @@ class ReceiptReminder extends Command
                     DingTalkController::send($touser->dtuserid, '',
                         $msg,
                         config('custom.dingtalk.agentidlist.approval'));
+
+//                if (!$this->option('nottomanager'))
+//                {
+//                    // 向销售经理发送消息
+//                    $salesmanager_hxold = Userold::where('user_hxold_id', $sohead->salesmanager_id)->first();
+//                    if (isset($salesmanager_hxold))
+//                    {
+//                        $salesmanager = User::where('id', $salesmanager_hxold->user_id)->first();
+//                        if (isset($salesmanager))
+//                            DingTalkController::send($salesmanager->dtuserid, '',
+//                                $msg,
+//                                config('custom.dingtalk.agentidlist.approval'));
+//                    }
+//
+//                }
+//
+//                // 向吴HL发送消息
+//                if ($toWuHL)
+//                {
+//                    $touser = User::where('email', 'wuhaolun@huaxing-east.com')->first();
+//                    if (isset($touser))
+//                        DingTalkController::send($touser->dtuserid, '',
+//                            $msg,
+//                            config('custom.dingtalk.agentidlist.approval'));
+//                }
+
+
 
             }
         }
