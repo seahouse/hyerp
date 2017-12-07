74abc5e8d9c112870be4442b1e798dee82b6df05
seahouse <seahouse@126.com>
2017-12-07 19:52:44 +0800

    test

---
+++ app/Http/Middleware/CORSMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CORSMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        header('Access-Control-Allow-Origin: *');
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Cookie, Accept',
+//            'Access-Control-Allow-Credentials' => 'true',
+        ];
+
+        $response = $next($request);
+        foreach($headers as $key => $value)
+            $response->header($key, $value);
+        return $response;
+    }
+}
