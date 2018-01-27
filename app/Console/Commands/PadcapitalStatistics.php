d013c06f766e827e1dd49fea8817d012baede801
seahouse <seahouse@126.com>
2018-01-27 15:47:41 +0800

    fix.

--- app/Console/Commands/PadcapitalStatistics.php
+++ app/Console/Commands/PadcapitalStatistics.php
@@ -55,9 +55,11 @@ class PadcapitalStatistics extends Command
 ////        });
 //        dd($data);
 
+        $padcapitalTotal = 0.0;
         $orderamountstatisticses = Vorderamountstatistics_hxold::where('padcapital', '>', 0.0)->orderBy('padcapital', 'desc')->get();
         foreach ($orderamountstatisticses as $orderamountstatistics)
         {
+            $padcapitalTotal += $orderamountstatistics->padcapital;
             $msg = $orderamountstatistics->projectjc == "" ? $orderamountstatistics->descrip : $orderamountstatistics->projectjc
                 . '(' .$orderamountstatistics->number . ')垫资'
                 . number_format($orderamountstatistics->padcapital, 4, '.', ',') . '元.';
@@ -93,67 +95,6 @@ class PadcapitalStatistics extends Command
                 }
             }
         }
-        dd('aaa');
-
-        $padcapitalTotal = 0.0;
-        $soheads = Salesorder_hxold::where('status', '<>', -10)->get();
-        foreach ($soheads as $sohead)
-        {
-            $this->info($sohead->id);
-
-            $receiptpayment = $sohead->receiptpayments->sum('amount')  * 10000;
-            $payment = $sohead->payments->sum('amount');
-            $poheadAmountBy7550 = array_first($sohead->getPoheadAmountBy7550())->poheadAmountBy7550;
-            $sohead_taxamount = isset($sohead->temTaxamountstatistics->sohead_taxamount) ? $sohead->temTaxamountstatistics->sohead_taxamount : 0.0;
-            $sohead_poheadtaxamount = isset($sohead->temTaxamountstatistics->sohead_poheadtaxamount) ? $sohead->temTaxamountstatistics->sohead_poheadtaxamount : 0.0;
-            $this->info('  ' . $receiptpayment);
-            $this->info('  ' . $payment);
-            $this->info('  ' . $poheadAmountBy7550);
-            $this->info('  ' . $sohead_taxamount);
-            $this->info('  ' . $sohead_poheadtaxamount);
-
-            if ($receiptpayment < ($payment + $poheadAmountBy7550))
-//            if ($receiptpayment < ($payment + $poheadAmountBy7550 + ($sohead_taxamount - $sohead_poheadtaxamount)))
-            {
-                $padcapital = $payment + $poheadAmountBy7550 - $receiptpayment;
-//                $padcapital = $payment + $poheadAmountBy7550 + ($sohead_taxamount - $sohead_poheadtaxamount) - $receiptpayment;
-                $padcapitalTotal += $padcapital;
-                $msg = ($sohead->projectjc == "" ? $sohead->descrip : $sohead->projectjc)
-                    . '(' .$sohead->number . ')垫资'
-                    . number_format($padcapital, 4, '.', ',') . '元.';
-                $this->info('  ' . $msg);
-                if ($this->option('debug'))
-                {
-                    $touser = User::where('email', $this->argument('useremail'))->first();
-                    if (isset($touser))
-                    {
-                        $data = [
-                            'userid'        => $touser->id,
-                            'msgcontent'    => urlencode($msg) ,
-                        ];
-
-                        DingTalkController::sendCorpMessageText(json_encode($data));
-                    }
-                }
-                else
-                {
-                    // send msg to Wuhl
-                    $userWuhl = User::where('email', 'wuhaolun@huaxing-east.com')->first();
-                    if (isset($userWuhl))
-                    {
-                        $data = [
-                            'userid'        => $userWuhl->id,
-                            'msgcontent'    => urlencode($msg) ,
-                        ];
-                        DingTalkController::sendCorpMessageText(json_encode($data));
-//                        DingTalkController::send($userWuhl->dtuserid, '',
-//                            $msg,
-//                            config('custom.dingtalk.agentidlist.erpmessage'));
-                    }
-                }
-            }
-        }
-
         $msg = '垫资总金额为' . number_format($padcapitalTotal, 4, '.', ',') . '元.';
         $this->info(number_format($padcapitalTotal, 4, '.', ','));
         if ($this->option('debug'))
@@ -171,7 +112,6 @@ class PadcapitalStatistics extends Command
         }
         else
         {
-            // send msg to Wuhl
             $userWuhl = User::where('email', 'wuhaolun@huaxing-east.com')->first();
             if (isset($userWuhl))
             {
@@ -182,5 +122,87 @@ class PadcapitalStatistics extends Command
                 DingTalkController::sendCorpMessageText(json_encode($data));
             }
         }
+//        dd('aaa');
+
+//        $padcapitalTotal = 0.0;
+//        $soheads = Salesorder_hxold::where('status', '<>', -10)->get();
+//        foreach ($soheads as $sohead)
+//        {
+//            $this->info($sohead->id);
+//
+//            $receiptpayment = $sohead->receiptpayments->sum('amount')  * 10000;
+//            $payment = $sohead->payments->sum('amount');
+//            $poheadAmountBy7550 = array_first($sohead->getPoheadAmountBy7550())->poheadAmountBy7550;
+//            $sohead_taxamount = isset($sohead->temTaxamountstatistics->sohead_taxamount) ? $sohead->temTaxamountstatistics->sohead_taxamount : 0.0;
+//            $sohead_poheadtaxamount = isset($sohead->temTaxamountstatistics->sohead_poheadtaxamount) ? $sohead->temTaxamountstatistics->sohead_poheadtaxamount : 0.0;
+//            $this->info('  ' . $receiptpayment);
+//            $this->info('  ' . $payment);
+//            $this->info('  ' . $poheadAmountBy7550);
+//            $this->info('  ' . $sohead_taxamount);
+//            $this->info('  ' . $sohead_poheadtaxamount);
+//
+//            if ($receiptpayment < ($payment + $poheadAmountBy7550))
+//            {
+//                $padcapital = $payment + $poheadAmountBy7550 - $receiptpayment;
+//                $padcapitalTotal += $padcapital;
+//                $msg = ($sohead->projectjc == "" ? $sohead->descrip : $sohead->projectjc)
+//                    . '(' .$sohead->number . ')垫资'
+//                    . number_format($padcapital, 4, '.', ',') . '元.';
+//                $this->info('  ' . $msg);
+//                if ($this->option('debug'))
+//                {
+//                    $touser = User::where('email', $this->argument('useremail'))->first();
+//                    if (isset($touser))
+//                    {
+//                        $data = [
+//                            'userid'        => $touser->id,
+//                            'msgcontent'    => urlencode($msg) ,
+//                        ];
+//
+//                        DingTalkController::sendCorpMessageText(json_encode($data));
+//                    }
+//                }
+//                else
+//                {
+//                    $userWuhl = User::where('email', 'wuhaolun@huaxing-east.com')->first();
+//                    if (isset($userWuhl))
+//                    {
+//                        $data = [
+//                            'userid'        => $userWuhl->id,
+//                            'msgcontent'    => urlencode($msg) ,
+//                        ];
+//                        DingTalkController::sendCorpMessageText(json_encode($data));
+//                    }
+//                }
+//            }
+//        }
+//
+//        $msg = '垫资总金额为' . number_format($padcapitalTotal, 4, '.', ',') . '元.';
+//        $this->info(number_format($padcapitalTotal, 4, '.', ','));
+//        if ($this->option('debug'))
+//        {
+//            $touser = User::where('email', $this->argument('useremail'))->first();
+//            if (isset($touser))
+//            {
+//                $data = [
+//                    'userid'        => $touser->id,
+//                    'msgcontent'    => urlencode($msg) ,
+//                ];
+//
+//                DingTalkController::sendCorpMessageText(json_encode($data));
+//            }
+//        }
+//        else
+//        {
+//            $userWuhl = User::where('email', 'wuhaolun@huaxing-east.com')->first();
+//            if (isset($userWuhl))
+//            {
+//                $data = [
+//                    'userid'        => $userWuhl->id,
+//                    'msgcontent'    => urlencode($msg) ,
+//                ];
+//                DingTalkController::sendCorpMessageText(json_encode($data));
+//            }
+//        }
     }
 }
