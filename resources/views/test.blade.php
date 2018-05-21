9cdc1463386ddac09fcdc224586c415280ac0e8c
seahouse <seahouse@126.com>
2018-05-21 18:14:16 +0800

    fix.

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,26 +6,18 @@ APP跳转
 
 @section('main')
 
-<html>
 
 
-<body style="margin: 0;overflow: hidden">
-<div id="tmPlayer" class="tmPlayer" style="height: 557px; width: 100%; height: 100%"></div>
-
-
-</body>
-</html>
-
-
-    @endsection
+@endsection
 
 @section('script')
     <script src="https://g.alicdn.com/dingding/open-develop/1.9.0/dingtalk.js"></script>
     <script type="text/javascript">
         jQuery(document).ready(function () {
             var appname = '{{ config('custom.dingtalk.thirdapps.aishu.android') }}';
-            if ({{ Agent::is('iPhone') }})
+            @if (Agent::is('iPhone'))
                 appname = '{{ config('custom.dingtalk.thirdapps.aishu.ios') }}';
+            @endif
 
             dd.config({
                 agentId: '{!! array_get($config, 'agentId') !!}', // 必填，微应用ID
@@ -38,9 +30,8 @@ APP跳转
                     'device.notification.confirm',
                     'biz.navigation.close'] // 必填，需要使用的jsapi列表
             });
-        });
 
-        dd.ready(function() {
+            dd.ready(function() {
 
 //            // if the page is not first history.back, exit it.
 //            if (history.length > 1)
@@ -56,42 +47,45 @@ APP跳转
 //            }
 
 
-            dd.device.launcher.launchApp({
-                app: appname, //iOS:应用scheme;Android:应用包名
+                dd.device.launcher.launchApp({
+                    app: appname, //iOS:应用scheme;Android:应用包名
 //                activity :'DetailActivity', //仅限Android，打开指定Activity，可不传。如果为空，就打开App的Main入口Activity
-                onSuccess : function(data) {
-                    /*
-                     {
-                     result: true //true 唤起成功 false 唤起失败
-                     }
-                     */
-                },
-                onFail : function(err) {
-                    alert('打开app失败.');
-                }
+                    onSuccess : function(data) {
+                        /*
+                         {
+                         result: true //true 唤起成功 false 唤起失败
+                         }
+                         */
+                    },
+                    onFail : function(err) {
+                        alert('打开app失败，请确认已经安装了爱数APP.');
+                    }
+                });
+            });
+
+            dd.error(function(error) {
+                alert('dd.error: ' + JSON.stringify(error));
+                {{ Cache::flush() }}       // add by seahouse, 2018/1/10
+                {{--
+                $.ajax({
+                    type:"GET",
+                    url:"{{ url('dingtalk/cacheflush') }}",
+                    error:function(xhr, ajaxOptions, thrownError){
+                        alert('error');
+                        alert(xhr.status);
+                        alert(xhr.responseText);
+                        alert(ajaxOptions);
+                        alert(thrownError);
+                    },
+                    success:function(){
+                        {{ Log::info('cacheflush') }}
+                    },
+                });
+                --}}
             });
         });
 
-        dd.error(function(error) {
-            alert('dd.error: ' + JSON.stringify(error));
-            {{ Cache::flush() }}       // add by seahouse, 2018/1/10
-            {{--
-            $.ajax({
-                type:"GET",
-                url:"{{ url('dingtalk/cacheflush') }}",
-                error:function(xhr, ajaxOptions, thrownError){
-                    alert('error');
-                    alert(xhr.status);
-                    alert(xhr.responseText);
-                    alert(ajaxOptions);
-                    alert(thrownError);
-                },
-                success:function(){
-                    {{ Log::info('cacheflush') }}
-                },
-            });
-            --}}
-        });
+
 
     </script>
 
