42335d411e16fa2c9b3f3aa75f1b9017dde3297e
seahouse <seahouse@126.com>
2019-04-09 15:32:59 +0800

    修改采购提醒逻辑。

--- app/Console/Commands/PurchaseReminder.php
+++ app/Console/Commands/PurchaseReminder.php
@@ -99,7 +99,7 @@ class PurchaseReminder extends Command
             }
             if ($bReminder)
             {
-                $msg = $sohead->projectjc . "(" . $sohead->number . ")已付预付款，但还未采购雾化器，请抓紧采购。";
+                $msg = $sohead->projectjc . "(" . $sohead->number . ")已付预付款，但还未采购雾化器，请抓紧采购。1";
 //                Log::info($msg);
                 $this->sendMsg($msg, 196);        // to LiuHM
                 $this->sendMsg($msg, $sohead->salesmanager_id);
@@ -134,7 +134,7 @@ class PurchaseReminder extends Command
 
                 if ($bReminder)
                 {
-                    $msg = $sohead->projectjc . "(" . $sohead->number . ")的下图审批包含栓接，但还未采购高强螺丝，请抓紧采购。";
+                    $msg = $sohead->projectjc . "(" . $sohead->number . ")的下图审批包含栓接，但还未采购高强螺丝，请抓紧采购。1";
 //                    Log::info($msg);
                     $this->sendMsg($msg, 196);        // to LiuHM
                     $this->sendMsg($msg, $sohead->salesmanager_id);
@@ -148,6 +148,7 @@ class PurchaseReminder extends Command
 
         // 刮板机 电伴热
         // 已录入开工报告
+        // 过滤已经掉通烟气，或者 72，或者环保验收
         $soheads = Salesorder_hxold::where('status', '<>', -10)->get();
         foreach ($soheads as $sohead)
         {
@@ -155,28 +156,55 @@ class PurchaseReminder extends Command
 //            Log::info($soheadstartreports->count());
             if ($soheadstartreports->count() > 0)
             {
-                $bReminderGBJ = true;
+                // 通烟气
+                $passgasDate = Carbon::parse($sohead->passgasDate);
+                $baseDate = Carbon::create(1900, 1, 1);
+                if ($passgasDate->gt($baseDate))
+                    continue;
+
+                // 72
+                $debugendDate = Carbon::parse($sohead->debugend_date);
+                $baseDate = Carbon::create(1900, 1, 1);
+                if ($debugendDate->gt($baseDate))
+                    continue;
+
+                // 环保验收
+                $environmentalProtectionCollectionDate = Carbon::parse($sohead->environmentalProtectionCollectionDate);
+                $baseDate = Carbon::create(1900, 1, 1);
+                if ($environmentalProtectionCollectionDate->gt($baseDate))
+                    continue;
+
+                $bReminderGBJ = false;
                 $bReminderDBR = true;
                 $this->info($sohead->id);
                 Log::info($sohead->id);
                 $poheads = $sohead->poheads;
                 foreach ($poheads as $pohead)
                 {
-//                    Log::info('productname: ' . $pohead->productname);
-                    if (strpos($pohead->productname, '刮板机') !== false)
+                    // 刮板机：仅针对喷雾订单
+                    $equipmenttypes = $sohead->equipmenttypes;
+                    foreach ($equipmenttypes as $equipmenttype)
                     {
-                        Log::info($pohead->productname);
-                        $bReminderGBJ = false;
+                        if (strpos($equipmenttype->equipmenttype_number, '喷雾') !== false)
+                        {
+                            $bReminderGBJ = true;
+                            if (strpos($pohead->productname, '刮板机') !== false)
+                            {
+//                        Log::info($pohead->productname);
+                                $bReminderGBJ = false;
+                            }
+                        }
                     }
+
                     if (strpos($pohead->productname, '电伴热') !== false)
                     {
-                        Log::info($pohead->productname);
+//                        Log::info($pohead->productname);
                         $bReminderDBR = false;
                     }
                 }
                 if ($bReminderGBJ)
                 {
-                    $msg = $sohead->projectjc . "(" . $sohead->number . ")已录入开工报告，但还未采购刮板机，请抓紧采购。";
+                    $msg = $sohead->projectjc . "(" . $sohead->number . ")已录入开工报告，但还未采购刮板机，请抓紧采购。1";
                     Log::info($msg);
                     $this->sendMsg($msg, 196);        // to LiuHM
                     $this->sendMsg($msg, $sohead->salesmanager_id);
@@ -187,7 +215,7 @@ class PurchaseReminder extends Command
                 }
                 if ($bReminderDBR)
                 {
-                    $msg = $sohead->projectjc . "(" . $sohead->number . ")已录入开工报告，但还未采购电伴热，请抓紧采购。";
+                    $msg = $sohead->projectjc . "(" . $sohead->number . ")已录入开工报告，但还未采购电伴热，请抓紧采购。1";
                     Log::info($msg);
                     $this->sendMsg($msg, 196);        // to LiuHM
                     $this->sendMsg($msg, $sohead->salesmanager_id);
