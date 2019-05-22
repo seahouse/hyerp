fb0c3ea4a0eae21bbef8f3a99c990fc235ca0e5e
seahouse <seahouse@126.com>
2019-05-22 15:43:56 +0800

    fix.

--- app/Http/Controllers/DingTalkController.php
+++ app/Http/Controllers/DingTalkController.php
@@ -1501,7 +1501,7 @@ class DingTalkController extends Controller
         $cc_list = config('custom.dingtalk.approversettings.issuedrawing.cc_list.' . $inputs['productioncompany']);
         $cc_list_default = config('custom.dingtalk.approversettings.issuedrawing.cc_list.default');
         $cc_list_designdepartment = config('custom.dingtalk.approversettings.issuedrawing.cc_list.designdepartment.' . $inputs['designdepartment']);
-        $cc_list .= empty($cc_list) ? $cc_list_default : $cc_list;
+        $cc_list .= empty($cc_list) ? $cc_list_default : '';
         $cc_list .= empty($cc_list) ? $cc_list_designdepartment : ',' . $cc_list_designdepartment;
         $cc_position = "FINISH";
 //        if (strlen($cc_list) == 0)
