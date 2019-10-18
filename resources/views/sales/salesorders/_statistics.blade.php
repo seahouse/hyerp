bf475a93739bbfad6da202b7f9b77181a0679b7f
seahouse <seahouse@126.com>
2019-10-18 17:23:33 +0800

    付款审批单，订单统计页面修改。

--- resources/views/sales/salesorders/_statistics.blade.php
+++ resources/views/sales/salesorders/_statistics.blade.php
@@ -15,11 +15,13 @@
 			@endif
 			<p>订单开票总金额：{{ $sohead->sotickets->sum('amount') }}万</p>
 			<?php $pohead_amount_total = $sohead->poheads->sum('amount'); ?>
+            <?php $pohead_amount_ticketed_total = $sohead->poheads->sum('amount_ticketed'); ?>
             <?php $poheadAmountBy7550 = array_first($sohead->getPoheadAmountBy7550())->poheadAmountBy7550; ?>
             <?php $sohead_taxamount = isset($sohead->temTaxamountstatistics->sohead_taxamount) ? $sohead->temTaxamountstatistics->sohead_taxamount : 0.0; ?>
             <?php $sohead_poheadtaxamount = isset($sohead->temTaxamountstatistics->sohead_poheadtaxamount) ? $sohead->temTaxamountstatistics->sohead_poheadtaxamount : 0.0; ?>
             <?php $sohead_poheadtaxamountby7550 = array_first($sohead->getPoheadTaxAmountBy7550())->poheadTaxAmountBy7550; ?>
 			<p>对应的采购订单合同金额总额：{{ number_format($pohead_amount_total / 10000.0, 4) }}万</p>		{{-- 似乎写到数据库视图中速度更快 --}}
+			<p>对应的采购订单开票总额：{{ number_format($pohead_amount_total / 10000.0, 4) }}万</p>
 			<p>公用订单分摊成本金额：{{ number_format($poheadAmountBy7550 / 10000.0, 4)  }}万</p>
 			<p>税差：{{ number_format(($sohead_taxamount - $sohead_poheadtaxamount - $sohead_poheadtaxamountby7550) / 10000.0, 4) }}万（含公摊税差{{ number_format($sohead_poheadtaxamountby7550 / 10000.0, 4) }}万）</p>
 			@if ($sohead->amount > 0.0)
@@ -39,6 +41,16 @@
 				<p>采购付款占销售订单比例：{{ number_format($pohead_amount_payment_total / ($sohead->amount * 10000.0) * 100.0, 2) }}%</p>
 			@else
 				<p>采购付款占销售订单比例：-</p>
+			@endif
+		@if (null !== $sohead->soheaddocs->where('type', 'swht')->first())
+			<p>商务合同：<a class="media" href="{{ config('custom.hxold.purchase_businesscontract_webdir') . 'swht/' . $sohead->id . '/' . $sohead->soheaddocs->where('type', 'swht')->first()->name }}" target="_blank" id="showPdf">{{ $sohead->soheaddocs->where('type', 'swht')->first()->name }}</a></p>
+		@else
+			<p>商务合同：无。</p>
+		@endif
+			@if (null !== $sohead->soheaddocs->where('type', 'jsxy')->first())
+				<p>技术协议：<a class="media" href="{{ config('custom.hxold.purchase_businesscontract_webdir') . 'jsxy/' . $sohead->id . '/' . $sohead->soheaddocs->where('type', 'jsxy')->first()->name }}" target="_blank" id="showPdf_jsxy">{{ $sohead->soheaddocs->where('type', 'jsxy')->first()->name }}</a></p>
+			@else
+				<p>技术协议：无。</p>
 			@endif
 		@endif
 	</div>
--- resources/views/sales/salesorders/mstatistics.blade.php
+++ resources/views/sales/salesorders/mstatistics.blade.php
@@ -4,8 +4,50 @@
 
 @section('main')
 	@include('sales.salesorders._statistics')
-@endsection
 
+	{{-- pdf 预览 --}}
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+						&times;
+					</button>
+					<h4 class="modal-title" id="myModalLabel">
+						PDF 预览标题
+					</h4>
+				</div>
+				<div class="modal-body" >
+					<a class="media" id="pdfContainer"
+					   @if (null !== $sohead->soheaddocs->where('type', 'swht')->first()) href="{{ config('custom.hxold.purchase_businesscontract_webdir') . 'swht/' . $sohead->id . '/' . $sohead->soheaddocs->where('type', 'swht')->first()->name }}" @else href="" @endif>
+
+					</a>
+				</div>
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal -->
+	</div>
+
+	<div class="modal fade" id="myModal_jsxy" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+						&times;
+					</button>
+					<h4 class="modal-title" id="myModalLabel">
+						PDF 预览标题
+					</h4>
+				</div>
+				<div class="modal-body" >
+					<a class="media" id="pdfContainer_jsxy"
+					   @if (null !== $sohead->soheaddocs->where('type', 'jsxy')->first()) href="{{ config('custom.hxold.purchase_businesscontract_webdir') . 'jsxy/' . $sohead->id . '/' . $sohead->soheaddocs->where('type', 'jsxy')->first()->name }}" @else href="" @endif>
+
+					</a>
+				</div>
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal -->
+	</div>
+@endsection
 
 @section('script')
 	<script type="text/javascript">
@@ -38,5 +80,85 @@
 			return false;
 	    };
 	</script>
+
+	@if (Agent::isDesktop())
+		<script src="/js/jquery.media.js"></script>
+
+		<script src="http://g.alicdn.com/dingding/dingtalk-pc-api/2.5.0/index.js"></script>
+		<script type="text/javascript">
+            jQuery(document).ready(function(e) {
+                $("a").attr("target", "_self");
+
+                {{-- 不需要config和ready，直接通过DingTalkPC.ua.isInDingTalk来判断 --}}
+
+                console.log(DingTalkPC.ua.isInDingTalk);
+                if (DingTalkPC.ua.isInDingTalk)
+                {
+                    $("#showPdf").click(function() {
+                        location.href = 'http://www.huaxing-east.cn:2015/pdfjs/build/generic/web/viewer.html?file=' + $("#showPdf").attr("href");
+                        return false;
+                    });
+
+                    $("#showPdf_jsxy").click(function() {
+                        location.href = 'http://www.huaxing-east.cn:2016/pdfjs/build/generic/web/viewer.html?file=' + $("#showPdf_jsxy").attr("href");
+                        return false;
+                    });
+					
+                }
+                else
+                {
+                    $(function() {
+                        $('#pdfContainer').media({width:'100%', height:800});
+                        $('#pdfContainer_jsxy').media({width:'100%', height:800});
+                    });
+
+                    $("#showPdf").click(function() {
+                        $('#myModal').modal();
+                        return false;
+                    });
+                    $("#showPdf_jsxy").click(function() {
+                        $('#myModal_jsxy').modal();
+                        return false;
+                    });
+                }
+
+
+                function showPdf() {
+                    var container = document.getElementById("container");
+                    container.style.display = "block";
+                    var url = 'http://www.huaxing-east.cn:2015/HxCgFiles/swht/7592/S30C-916092615220%EF%BC%88%E5%8D%8E%E4%BA%9A%E7%94%B5%E8%A2%8B%E9%99%A4%E5%B0%98%E5%90%88%E5%90%8C%EF%BC%89.pdf';
+                    PDFJS.workerSrc = '/js/pdf.worker.min.js';
+                    PDFJS.getDocument(url).then(function getPdfHelloWorld(pdf) {
+                        pdf.getPage(1).then(function getPageHelloWorld(page) {
+                            var scale = 1;
+                            var viewport = page.getViewport(scale);
+                            var canvas = document.getElementById('the-canvas');
+                            var context = canvas.getContext('2d');
+                            canvas.height = viewport.height;
+                            canvas.width = viewport.width;
+                            var renderContext = {
+                                canvasContext: context,
+                                viewport: viewport
+                            };
+                            page.render(renderContext);
+                        });
+                    });
+                }
+
+                $("#btnTest").click(function() {
+                    showPdf();
+
+                });
+            });
+		</script>
+	@elseif(Agent::isMobile())
+		<script src="https://g.alicdn.com/ilw/ding/0.7.5/scripts/dingtalk.js"></script>
+		<script type="text/javascript">
+            $("#showPdf").click(function() {
+                location.href = 'http://www.huaxing-east.cn:2015/pdfjs/build/generic/web/viewer.html?file=' + $("#showPdf").attr("href");
+                return false;
+            });
+		</script>
+	@endif
 @endsection
 
