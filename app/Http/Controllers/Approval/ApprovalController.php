4db9399ad9932f69a289a191cd735b1a1d899b8e
seahouse <seahouse@126.com>
2019-10-28 22:32:28 +0800

    fix.

--- app/Http/Controllers/Approval/ApprovalController.php
+++ app/Http/Controllers/Approval/ApprovalController.php
@@ -235,7 +235,7 @@ class ApprovalController extends Controller
         // 特殊情况：
         // 如果是候S，可以看到供应商付款的第4级审批
         if ($user->id == 123)
-            $ids_approversetting->merge([11]);
+            $ids_approversetting = $ids_approversetting->merge([11]);
 
         // 如果审批设置中没有设置人员，而是设置了部门和职位，那么也要加进去
         $ids_approversetting2 = [];
