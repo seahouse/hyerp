9be2e78cfe03b4b8edd80b28775906618d2c350c
luzenggui <luren6511>
2019-12-09 09:53:36 +0800

    更新供应商付款中的出库类成本比例

--- resources/views/sales/salesorders/_statistics.blade.php
+++ resources/views/sales/salesorders/_statistics.blade.php
@@ -40,7 +40,7 @@
 			<p>出库物品税差：{{number_format(($sohead_taxamount - $warehousetaxcost ) / 10000.0, 4)}}万</p>
 			<p>无入库记录合同金额总额：{{number_format($nowarehousecost/ 10000.0, 4)}}万</p>
 			<p>无入库记录物品税差：{{number_format(($sohead_taxamount - $nowarehousetaxcost) / 10000.0, 4)}}万</p>
-			<p>出库类成本比例：{{number_format(($warehousecost  + $sohead_taxamount - $nowarehousetaxcost) / ($sohead->amount * 10000.0) * 100.0, 2)}}%</p>
+			<p>出库类成本比例：{{number_format(($warehousecost  + $nowarehousecost + $sohead_taxamount - $nowarehousetaxcost-$warehousetaxcost) / ($sohead->amount * 10000.0) * 100.0, 2)}}%</p>
 			<hr style="border-top-color:rgba(0,0,0,1);" >
 		@if (isset($sohead->project->id))
 			<a href="{{ URL::to('/sales/projects/' . $sohead->project->id . '/mstatistics') }}" target="_blank" class="btn btn-default btn-sm">备注</a>
