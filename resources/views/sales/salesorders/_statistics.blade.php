b636e2ef5e96305e7d82a6148d2c950de24e392c
luzenggui <luren2007@163.com>
2020-01-06 09:05:30 +0800

    修改供应商付款的未入库金额和出库金额、出库成本比例

--- resources/views/sales/salesorders/_statistics.blade.php
+++ resources/views/sales/salesorders/_statistics.blade.php
@@ -41,11 +41,11 @@
 			<a href="{{ URL::to('/sales/projects/'.$sohead->id.'/otherwarehousedetail/') }}" class="btn btn-default btn-sm" target="_blank">出到其他订单明细</a>
 			<a href="{{ URL::to('/sales/projects/'.$sohead->id.'/fromotherwarehousedetail/') }}" class="btn btn-default btn-sm" target="_blank">从其他订单来明细</a>
 			<a href="{{ URL::to('/sales/projects/'.$sohead->id.'/leftwarehousedetail/') }}" class="btn btn-default btn-sm" target="_blank">剩余订单库存明细</a>
-			<p>出库物品税差：{{number_format(($sohead_taxamount - $warehousetaxcost ) / 10000.0, 4)}}万</p>
+			<p>出库物品税差：{{number_format(( $warehousetaxcost ) / 10000.0, 4)}}万</p>
 			<p>无入库记录合同金额总额：{{number_format($nowarehousecost/ 10000.0, 4)}}万</p>
-			<p>无入库记录物品税差：{{number_format(($sohead_taxamount - $nowarehousetaxcost) / 10000.0, 4)}}万</p>
+			<p>无入库记录物品税差：{{number_format(( $nowarehousetaxcost) / 10000.0, 4)}}万</p>
 			@if ($sohead->amount > 0.0)
-				<p>出库类成本比例：{{number_format(($warehousecost  + $nowarehousecost + $sohead_taxamount + $nowarehousetaxcost-$warehousetaxcost) / ($sohead->amount * 10000.0) * 100.0, 2)}}%</p>
+				<p>出库类成本比例：{{number_format(($warehousecost  + $nowarehousecost + $sohead_taxamount - $nowarehousetaxcost-$warehousetaxcost) / ($sohead->amount * 10000.0) * 100.0, 2)}}%</p>
 			@else
 				<p>出库类成本比例：-</p>
 			@endif
