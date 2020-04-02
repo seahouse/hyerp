fbbeb84a73d47bfa14b3058679b73a1d177961b5
luzenggui <luren2007@163.com>
2020-04-02 15:53:19 +0800

    update 工资条

--- app/Http/Controllers/System/SalarysheetController.php
+++ app/Http/Controllers/System/SalarysheetController.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use Excel, Log,Auth;
+use Excel, Log,Auth,DB,Datatables;
 
 class SalarysheetController extends Controller
 {
