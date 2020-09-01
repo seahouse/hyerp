1075033550cc29c5a5a18dddacf8a4d814eaa6d0
seahouse <seahouse@126.com>
2020-09-01 15:14:43 +0800

    fix.

--- resources/views/purchase/purchaseorders/index_hx.blade.php
+++ resources/views/purchase/purchaseorders/index_hx.blade.php
@@ -116,6 +116,7 @@
                             @else -
                             @endif {{ ')' }}
                         @else
+                            {{ '-(' }}
                             {{-- 当 对公付款审批 的类型是“安装合同安装费付款”，且采购商品名称是“钢结构安装”，开放已付金额百分比给发起人 --}}
                             @if (strpos($purchaseorder->productname, '钢结构安装') >= 0)
                                 @if ($purchaseorder->corporatepayments()->where('amounttype', '安装合同安装费付款')->where('status', '>=', 0)->where('applicant_id', Auth::user()->id)->count())
@@ -124,6 +125,7 @@
                                     @endif
                                 @endif
                             @endif
+                            {{ ')' }}
                         @endcan
                     </td>
                     @can('purchase_purchaseorder_viewamount')
