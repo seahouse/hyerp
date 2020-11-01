9f35b722a6f5be99ac6349636a510ff7acf99b0d
seahouse <seahouse@126.com>
2020-11-01 10:28:20 +0800

    fix.

--- app/Http/Controllers/Approval/PaymentrequestsController.php
+++ app/Http/Controllers/Approval/PaymentrequestsController.php
@@ -52,17 +52,12 @@ class PaymentrequestsController extends Controller
         $approvalstatus = $request->input('approvalstatus', '');
         $paymentstatus = $request->input('paymentstatus');
         $inputs = $request->all();
-//        dd($inputs);
         $items = $this->searchrequest($request);
-//        dd($items->count());
-        $totalamount = $items->get()->sum('amount');
+        // 临时去掉汇总，由于数据太大，获取数据失败。后面重新实现获取汇总值
+        $totalamount = 0.0;
+//        $totalamount = $items->get()->sum('amount');
 //        $totalamount = Paymentrequest::sum('amount');
         $paymentrequests = $items->paginate(10);
-//        dd($paymentrequests);
-//        if (null !== request('key'))
-//            $paymentrequests = $this->searchrequest($request)->paginate(10);
-//        else
-//            $paymentrequests = Paymentrequest::latest('created_at')->paginate(10);
         $purchaseorders = Purchaseorder_hxold::whereIn('id', $paymentrequests->pluck('pohead_id'))->get();
 
 //        return view('approval.paymentrequests.index');
@@ -204,7 +199,6 @@ class PaymentrequestsController extends Controller
         }
 //        dd($purchaseorder_ids);
         $query = Paymentrequest::latest('paymentrequests.created_at');
-//        dd($query);
         if (strlen($key) > 0)
         {
             $query->where(function($query) use ($supplier_ids, $purchaseorder_ids, $key) {
@@ -334,7 +328,7 @@ class PaymentrequestsController extends Controller
         }
 
         $paymentrequests = $query->select('paymentrequests.*');
-         dd($paymentrequests->count());
+//         dd($paymentrequests->count());
 
         // $purchaseorders = Purchaseorder_hxold::whereIn('id', $paymentrequests->pluck('pohead_id'))->get();
 //         dd($paymentrequests);
@@ -1752,5 +1746,17 @@ class PaymentrequestsController extends Controller
 
         return redirect('/pdfjs/build/generic/web/viewer.html?file=http://www.huaxing-east.cn:2015/HxCgFiles/swht/7592/S30C-916092615220%EF%BC%88%E5%8D%8E%E4%BA%9A%E7%94%B5%E8%A2%8B%E9%99%A4%E5%B0%98%E5%90%88%E5%90%8C%EF%BC%89.pdf') ;
 //        view('/pdfjs/build/generic/web/viewer.html?file=compressed.tracemonkey-pldi-09.pdf');
+    }
+
+    // 打印多个记录的页面
+    public function printmulti(Request $request)
+    {
+        if ($request->has('ids') && strlen($request->input('ids')))
+        {
+            $ids = explode(',', $request->input('ids'));
+            $paymentrequests = Paymentrequest::whereIn('id', $ids)->get();
+
+            return view('approval.paymentrequests.printmulti', compact('paymentrequests'));
+        }
     }
 }
